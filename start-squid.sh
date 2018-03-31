b914f16240e2790f357e433e89176e1d09c935f4
jrhoward <jrhowd@gmail.com>
2018-03-31 13:11:31 +1100

    removed squid.pid on start up script

--- start-squid.sh
+++ start-squid.sh
@@ -19,5 +19,7 @@ if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
   echo "Initializing cache..."
   squid -N -f /etc/squid/squid.conf -z
 fi
+rm -f /var/run/squid.pid || true
+
 echo "Starting squid..."
 exec squid -f /etc/squid/squid.conf -NYCd 1
